358e023acd187198babd428046d6e37f2137594d
Federico Bello <fe.debello13@gmail.com>
2025-12-01 16:03:56 -0300

    feat: model preprocessing
    fix: preprocessing

--- challenge/model.py
+++ challenge/model.py
@@ -1,19 +1,84 @@
 import pandas as pd
 
 from typing import Tuple, Union, List
+from datetime import datetime
+import numpy as np
+
+THRESHOLD_IN_MINUTES = 15
+
 
 class DelayModel:
+    _TOP_10_FEATURES = [
+        "OPERA_Latin American Wings",
+        "MES_7",
+        "MES_10",
+        "OPERA_Grupo LATAM",
+        "MES_12",
+        "TIPOVUELO_I",
+        "MES_4",
+        "MES_11",
+        "OPERA_Sky Airline",
+        "OPERA_Copa Air",
+    ]
+    _THRESHOLD_IN_MINUTES = 15
 
-    def __init__(
-        self
-    ):
-        self._model = None # Model should be saved in this attribute.
+    def __init__(self):
+        self._model = None  # Model should be saved in this attribute.
+
+    def _get_period_day(self, date):
+        date_time = datetime.strptime(date, "%Y-%m-%d %H:%M:%S").time()
+        morning_min = datetime.strptime("05:00", "%H:%M").time()
+        morning_max = datetime.strptime("11:59", "%H:%M").time()
+        afternoon_min = datetime.strptime("12:00", "%H:%M").time()
+        afternoon_max = datetime.strptime("18:59", "%H:%M").time()
+        evening_min = datetime.strptime("19:00", "%H:%M").time()
+        evening_max = datetime.strptime("23:59", "%H:%M").time()
+        night_min = datetime.strptime("00:00", "%H:%M").time()
+        night_max = datetime.strptime("4:59", "%H:%M").time()
+
+        if date_time > morning_min and date_time < morning_max:
+            return "mañana"
+        elif date_time > afternoon_min and date_time < afternoon_max:
+            return "tarde"
+        elif (date_time > evening_min and date_time < evening_max) or (
+            date_time > night_min and date_time < night_max
+        ):
+            return "noche"
+
+    def _is_high_season(self, fecha):
+        fecha_año = int(fecha.split("-")[0])
+        fecha = datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S")
+        range1_min = datetime.strptime("15-Dec", "%d-%b").replace(year=fecha_año)
+        range1_max = datetime.strptime("31-Dec", "%d-%b").replace(year=fecha_año)
+        range2_min = datetime.strptime("1-Jan", "%d-%b").replace(year=fecha_año)
+        range2_max = datetime.strptime("3-Mar", "%d-%b").replace(year=fecha_año)
+        range3_min = datetime.strptime("15-Jul", "%d-%b").replace(year=fecha_año)
+        range3_max = datetime.strptime("31-Jul", "%d-%b").replace(year=fecha_año)
+        range4_min = datetime.strptime("11-Sep", "%d-%b").replace(year=fecha_año)
+        range4_max = datetime.strptime("30-Sep", "%d-%b").replace(year=fecha_año)
+
+        if (
+            (fecha >= range1_min and fecha <= range1_max)
+            or (fecha >= range2_min and fecha <= range2_max)
+            or (fecha >= range3_min and fecha <= range3_max)
+            or (fecha >= range4_min and fecha <= range4_max)
+        ):
+            return 1
+        else:
+            return 0
+
+    def _get_min_diff(self, data):
+        fecha_o = datetime.strptime(data["Fecha-O"], "%Y-%m-%d %H:%M:%S")
+        fecha_i = datetime.strptime(data["Fecha-I"], "%Y-%m-%d %H:%M:%S")
+        min_diff = ((fecha_o - fecha_i).total_seconds()) / 60
+        return min_diff
+
+    def _get_top_features(self, data: pd.DataFrame) -> pd.DataFrame:
+        return data[self._TOP_10_FEATURES]
 
     def preprocess(
-        self,
-        data: pd.DataFrame,
-        target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+        self, data: pd.DataFrame, target_column: str = None
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
         """
         Prepare raw data for training or predict.
 
@@ -26,13 +91,27 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+        data["period_day"] = data["Fecha-I"].apply(self._get_period_day)
+        data["high_season"] = data["Fecha-I"].apply(self._is_high_season)
+        data["min_diff"] = data.apply(self._get_min_diff, axis="columns")
+        data["delay"] = np.where(data["min_diff"] > self._THRESHOLD_IN_MINUTES, 1, 0)
 
-    def fit(
-        self,
-        features: pd.DataFrame,
-        target: pd.DataFrame
-    ) -> None:
+        features = pd.concat(
+            [
+                pd.get_dummies(data["OPERA"], prefix="OPERA"),
+                pd.get_dummies(data["TIPOVUELO"], prefix="TIPOVUELO"),
+                pd.get_dummies(data["MES"], prefix="MES"),
+            ],
+            axis="columns",
+        )
+        features = self._get_top_features(features)
+        if not target_column:
+            return features
+
+        target = pd.DataFrame(data[target_column])
+        return features, target
+
+    def fit(self, features: pd.DataFrame, target: pd.DataFrame) -> None:
         """
         Fit model with preprocessed data.
 
@@ -42,16 +121,13 @@ class DelayModel:
         """
         return
 
-    def predict(
-        self,
-        features: pd.DataFrame
-    ) -> List[int]:
+    def predict(self, features: pd.DataFrame) -> List[int]:
         """
         Predict delays for new flights.
 
         Args:
             features (pd.DataFrame): preprocessed data.
-        
+
         Returns:
             (List[int]): predicted targets.
         """
